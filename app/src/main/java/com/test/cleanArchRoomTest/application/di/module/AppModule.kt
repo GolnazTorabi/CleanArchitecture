92122ea29798a0d7b1f42a010e7a95bb2415ad84
GolnazTorabi <g.torabi76@gmail.com>
2020-10-19 14:14:43 +0330

    provide db into app module

--- app/src/main/java/com/test/cleanArchRoomTest/application/di/module/AppModule.kt
+++ app/src/main/java/com/test/cleanArchRoomTest/application/di/module/AppModule.kt
@@ -3,10 +3,12 @@ package com.test.cleanArchRoomTest.application.di.module
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.test.cleanArchRoomTest.application.di.component.ViewModelSubComponent
 import com.test.cleanArchRoomTest.application.di.factory.ViewModelFactory
 import com.test.cleanArchRoomTest.data.ApiInterface
+import com.test.cleanArchRoomTest.data.database.AppDatabase
 import com.test.cleanArchRoomTest.data.repository.CharactersRepositoryImpl
 import com.test.cleanArchRoomTest.domain.repository.CharactersRepository
 import dagger.Module
@@ -70,12 +72,12 @@ internal class AppModule {
     }
 */
 
-    /*  @Singleton
+      @Singleton
     @Provides
     fun provideRoomDatabase(application: Application): AppDatabase {
-        return Room.databaseBuilder(application.applicationContext, AppDatabase::class.java, "VANDAR-DB")
+        return Room.databaseBuilder(application.applicationContext, AppDatabase::class.java, "AppData")
             .allowMainThreadQueries().fallbackToDestructiveMigration().build()
-    }*/
+    }
 
     @Provides
     fun provideUserRepository(repo: CharactersRepositoryImpl): CharactersRepository = repo
